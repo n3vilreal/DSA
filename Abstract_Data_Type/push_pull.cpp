9749fd17654d6b7cdd4e805f1c0f465ac8e00668
n3vilreal <nebil.241423@ncit.edu.np>
2025-11-23 20:45:51 +0545

    Push Pull operaction

---
+++ Abstract_Data_Type/push_pull.cpp
@@ -0,0 +1,84 @@
+//Program to choose to push or pull data to stack
+#include<iostream>
+#define MAX 10
+using namespace std;
+class Stack
+{
+private:
+    int top=-1;
+    int arr[MAX];
+public:
+    void push(int data)
+    {
+        if(top==MAX-1)
+        {
+            cout<<"Stack is full";
+        }
+        else
+        {
+            top+=1;
+            arr[top] = data;
+        }
+    }
+    void pop()
+    {
+        if(top==-1)
+        {
+            cout<<"Stack is empty";
+        }
+        else
+        {
+            cout<<arr[top]<<" is deleted"<<endl;
+            top-=1;
+        }
+    }
+    void displayStack()
+    {   
+        cout<<"Stack: ";
+        for(int i=0; i<=top; i++)
+        {
+            cout<<arr[i]<<"\t";
+        }
+    }
+};
+int main()
+{   
+    Stack s;
+    int choice, n, value;
+    while(true)
+    {
+        cout<<"Menu"<<endl;
+    cout<<"1: Push"<<endl<<"2: Pop"<<endl<<"3: Exit"<<endl;
+    cout<<"Enter your choice: ";
+    cin>>choice;
+    switch(choice)
+    {
+        case 1:
+            cout<<"How many values? (Less than 10): ";
+            cin>>n;
+            for(int i=1;i<=n;i++)
+            {
+                cout<<"Enter value "<<i<<": ";
+                cin>>value;
+                s.push(value);
+            }
+            s.displayStack();
+            cout<<endl;
+            break;
+        case 2:
+            cout<<endl<<"How many values to delete: ";
+            cin>>value;
+            for(int i=0;i<value;i++)
+            {
+                s.pop();
+            }
+            break;
+        case 3:
+            cout<<"Exiting Program";
+            exit(1);
+        default:
+            cout<<"Invalid input";
+    }
+    }
+    return 0;
+}
