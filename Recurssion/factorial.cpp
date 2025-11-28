56e5e5386a25db975941344b2040862ef3590259
n3vilreal <nebil.241423@ncit.edu.np>
2025-11-28 09:32:41 +0545

    fact cannot be calculated

--- Recurssion/factorial.cpp
+++ Recurssion/factorial.cpp
@@ -6,12 +6,18 @@ int main()
     int num, fact;
     cout<<"Enter a number: ";
     cin>>num;
+    if(num == -1)
+    {
+        cout<<"Factorial cannot be calculated.";
+        exit(0);
+    }
     fact = factorial(num);
     cout<<num<<"! = "<<fact;
     return 0;
 }  
 int factorial(int a)
 {
+    
     if(a == 0 || a == 1)
     {
         return 1;
