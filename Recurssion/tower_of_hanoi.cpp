659addda47d7e2ba2bdb6927cc9b299cfb80147b
n3vilreal <nebil.241423@ncit.edu.np>
2025-11-28 09:57:31 +0545

    steps toh

--- Recurssion/tower_of_hanoi.cpp
+++ Recurssion/tower_of_hanoi.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
+#include<math.h>
 using namespace std;
 void towerOfHanoi(int,char,char,char);
 int main()
 {
-    int n;
+    int n, steps;
     cout << "Enter number of disks: ";
     cin >> n;
     cout<<"A = source, B = auxiliary, C = destination"<<endl;
     towerOfHanoi(n, 'A', 'B', 'C');
-
+    steps = pow(2,n) - 1;
+    cout<<endl<<"Number of steps:"<<steps;
     return 0;
 }
 void towerOfHanoi(int n, char source, char auxiliary, char destination)
-{
+{   
     if (n == 1)
     {
         cout << "Move disk 1 from " << source << " to " << destination << endl;
